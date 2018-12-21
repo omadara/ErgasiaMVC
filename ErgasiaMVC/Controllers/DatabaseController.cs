d4c42c12ffb16b3c9b3306ef7ddcf5e1037b8de9
MadProgrammerGR <giorgozombo@yahoo.gr>
2018-12-21 23:06:58 +0200

    refactor

--- ErgasiaMVC/Controllers/DatabaseController.cs
+++ ErgasiaMVC/Controllers/DatabaseController.cs
@@ -1,6 +1,7 @@
 ﻿using ErgasiaMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
@@ -9,18 +10,34 @@ using System.Web.Mvc;
 
 namespace ErgasiaMVC.Controllers
 {
-    public class RestoreDBController : Controller
+    public class DatabaseController : Controller
     {
-        // GET: RestoreDB
         public ActionResult Index() {
             return View();
         }
 
+        public ActionResult DownloadBackupFile() {
+            string backup_dir = Path.Combine(Path.GetTempPath(), "mvc_ergasia_db");
+            string backup_file = "pubs-" + DateTime.Now.ToString("yyyy-MM-dd_HHmmss") + ".Bak";
+
+            Directory.CreateDirectory(backup_dir);
+
+            pubsEntities db = new pubsEntities();
+            SqlConnection sqlCon = (SqlConnection)(db.Database.Connection);
+            sqlCon.Open();
+            SqlCommand sqlCmd = new SqlCommand("backup database pubs to disk='" + Path.Combine(backup_dir, backup_file) + "'", sqlCon);
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            db.Dispose();
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(Path.Combine(backup_dir, backup_file));
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, backup_file);
+        }
+        
 
         [HttpPost]
-        public ActionResult Index(HttpPostedFileBase file) {
+        public ActionResult Restore(HttpPostedFileBase file) {
             if (file != null && file.ContentLength > 0) {
-
                 var fileName = Path.GetFileName(file.FileName);
                 var path = Path.Combine(Path.GetTempPath(), fileName);
                 file.SaveAs(path);
@@ -40,12 +57,12 @@ namespace ErgasiaMVC.Controllers
                 sqlCon.Close();
 
                 ViewBag.message = "Successful";
-                ViewBag.cssClass = "text-success";
+                ViewBag.cssClass = "alert alert-success";
             }else {
                 ViewBag.message = "Invalid file";
-                ViewBag.cssClass = "text-danger";
+                ViewBag.cssClass = "alert alert-danger";
             }
-            return View();
+            return View("Index");
         }
     }
 }
